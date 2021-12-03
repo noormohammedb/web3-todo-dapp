37cbcd0f03f696fc7e5218dfb87782eb80c84932
Noor Mohammed <mail@noormohammed.tech>
2021-12-03 16:05:24 +0530

    dev setup
    fix solidity version
    todolist contract
    migration file
    connected ganache
    ignored build

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,5 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
+	uint256 taskCound = 0;
 }
--- truffle-config.js
+++ truffle-config.js
@@ -46,6 +46,13 @@ module.exports = {
     //  port: 8545,            // Standard Ethereum port (default: none)
     //  network_id: "*",       // Any network (default: none)
     // },
+
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -81,13 +88,13 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.10",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      optimizer: {
+        enabled: false,
+        runs: 200
+      },
       //  evmVersion: "byzantium"
       // }
     }
@@ -104,13 +111,13 @@ module.exports = {
   // $ truffle migrate --reset --compile-all
   //
   // db: {
-    // enabled: false,
-    // host: "127.0.0.1",
-    // adapter: {
-    //   name: "sqlite",
-    //   settings: {
-    //     directory: ".db"
-    //   }
-    // }
+  // enabled: false,
+  // host: "127.0.0.1",
+  // adapter: {
+  //   name: "sqlite",
+  //   settings: {
+  //     directory: ".db"
+  //   }
+  // }
   // }
 };
