5ded44e7cb2ad17ccba1588421aa5d7dfad6bdd7
Noor Mohammed <mail@noormohammed.tech>
2021-12-04 12:32:13 +0530

    Smart Contract
    TodoList basic struct and create

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,39 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-	uint256 taskCound = 0;
+	uint256 taskCount = 0;
+
+	struct Task {
+		uint256 id;
+		string content;
+		bool completed;
+	}
+
+	mapping(uint256 => Task) public tasks;
+
+	constructor() public {
+		createTask("test");
+	}
+
+	function createTask(string memory newTask) public {
+		tasks[taskCount] = Task(taskCount, newTask, false);
+		taskCount++;
+	}
+
+	function getAllTasksIndex() public view returns (uint256) {
+		return taskCount;
+	}
+
+	function getTaskAtIndex(uint256 index)
+		public
+		view
+		returns (
+			uint256,
+			string memory,
+			bool
+		)
+	{
+		Task memory tempTask = tasks[index];
+		return (tempTask.id, tempTask.content, tempTask.completed);
+	}
 }
